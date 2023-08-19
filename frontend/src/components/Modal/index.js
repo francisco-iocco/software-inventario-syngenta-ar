6375aa1a6e915016a831394daa3a074ea67f84f8
Francisco Iocco <franciscoiocco6@gmail.com>
2023-08-19 00:20:51 -0300

    Add actions to the modal

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,16 +1,53 @@
+import { useState } from "react";
 import { createPortal } from "react-dom";
-import { StyledModal, Container } from "./styles";
+import { StyledModal, Container, Actions, Button, CloseContainer } from "./styles";
+import { IconX } from "@tabler/icons-react";
 
-function Modal() {
+function Modal({ onClose }) {
+  const [action, setAction] = useState("add");
+  const [animation, setAnimation] = useState("");
+
+  const handleClose = () => {
+    setAnimation("close-modal");
+    // setTimeout to wait for animation before closing
+    setTimeout(onClose, 500);
+  };
+
   return (
-    <StyledModal>
-      <Container>
-        <div></div>
+    <StyledModal animation={animation}>
+      <Container animation={animation}>
+        <div>
+          <Actions>
+            <Button
+              isActive={action === "add" ? "true" : "false"}
+              onClick={() => setAction("add")}
+              desc="add"
+            >
+              Agregar
+            </Button>
+            <Button
+              isActive={action === "deliver" ? "true" : "false"}
+              onClick={() => setAction("deliver")}
+              desc="deliver"
+            >
+              Entregar
+            </Button>
+            <CloseContainer>
+              <button>
+                <IconX size="1em" onClick={handleClose} />
+              </button>
+            </CloseContainer>
+          </Actions>
+        </div>
       </Container>
     </StyledModal>
   );
 }
 
-export default function ModalPortal() {
-  return createPortal(<Modal />, document.getElementById("modal"));
+// Creating portal so that modal renders above everything
+export default function ModalPortal({ onClose }) {
+  return createPortal(
+    <Modal onClose={onClose} />,
+    document.getElementById("modal")
+  );
 }
--- frontend/src/components/Modal/styles.js
+++ frontend/src/components/Modal/styles.js
@@ -1,15 +1,18 @@
 import { styled } from "styled-components";
 
 export const StyledModal = styled.div`
-  background-color: rgba(0, 0, 0, 0.6);
+  background-color: ${({ animation }) =>
+    animation === "close-modal" ? "rgba(0, 0, 0, .0)" : "rgba(0, 0, 0, .6)"};
   height: 100%;
   position: absolute;
+  transition: background-color .5s;
   width: 100%;
   z-index: 100;
 `;
 
 export const Container = styled.div`
-  animation: showUp 0.5s forwards;
+  animation: ${({ animation }) =>
+    animation === "close-modal" ? "hideDown" : "showUp"} 0.5s forwards;
   display: grid;
   height: 100%;
   place-items: center;
@@ -18,8 +21,10 @@ export const Container = styled.div`
 
   & > div {
     background-color: #fff;
+    border-radius: 0 0 15px 15px;
     height: 450px;
     max-width: 500px;
+    position: relative;
     width: 90%;
   }
 
@@ -30,5 +35,50 @@ export const Container = styled.div`
     to {
       transform: translateY(0);
     }
+  }
+
+  @keyframes hideDown {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(100%);
+    }
+  }
+`;
+
+export const Actions = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: 50px 50px;
+  position: absolute;
+  transform: translateY(-50%);
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  background-color: ${({ isActive }) =>
+    isActive === "true" ? "#fff" : "#eee"};
+  border-radius: 10px 10px 0 0;
+  border: none;
+  color: ${({ isActive, desc }) => {
+    if (isActive === "false") return "#ddd";
+    if (desc === "add") return "darkgreen";
+    if (desc === "deliver") return "#613fe5";
+  }};
+  font-weight: 600;
+  padding: 10px 0;
+`;
+
+export const CloseContainer = styled.div`
+  grid-column: 1 / span 2;
+  
+  button {
+    background-color: transparent;
+    border: none;
+    color: #eee;
+    float: right;
+    font-size: 2em;
+    margin: 10px;
   }
 `;
