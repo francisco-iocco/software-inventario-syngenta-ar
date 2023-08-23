f8ac8419cc0e73493724c3321084006e12810dae
Francisco Iocco <franciscoiocco6@gmail.com>
2023-08-23 09:22:08 -0300

    Add GET endpoint for several resources and another one for a unique resource

--- backend/index.js
+++ backend/index.js
@@ -26,24 +26,38 @@ app.post("/gadgets", upload.single("image"), async (req, res) => {
 
   // Verifying if every field has been sent (and if its format is correct)
 
-  if(!body.name) {
-    return res.status(400).send({ err: "El nombre del dispositivo es obligatorio" });
-  };
-
-  if(!body.barcode) {
-    return res.status(400).send({ err: "El código de barras del dispositivo es obligatorio" });
-  } else if(!parseInt(body.barcode)) {
-    return res.status(400).send({ err: "El código de barras debe ser un conjunto de números" });
+  if (!body.name) {
+    return res
+      .status(400)
+      .send({ err: "El nombre del dispositivo es obligatorio" });
   }
 
-  if(!body.ownedQuantity > 0) {
-    return res.status(400).send({ err: "La cantidad de dispositivos debe ser mayor que 0" });
-  } else if(!parseInt(body.barcode)) {
-    return res.status(400).send({ err: "La cantidad de dispositivos propios debe ser un número" });
+  if (!body.barcode) {
+    return res
+      .status(400)
+      .send({ err: "El código de barras del dispositivo es obligatorio" });
+  } else if (!parseInt(body.barcode)) {
+    return res
+      .status(400)
+      .send({ err: "El código de barras debe ser un conjunto de números" });
   }
 
-  if(!file?.buffer || !file?.mimetype) {
-    return res.status(400).send({ err: "La imagen del dispositivo el obligatoria, al igual que su tipo MIME" });
+  if (!body.ownedQuantity > 0) {
+    return res
+      .status(400)
+      .send({ err: "La cantidad de dispositivos debe ser mayor que 0" });
+  } else if (!parseInt(body.barcode)) {
+    return res
+      .status(400)
+      .send({ err: "La cantidad de dispositivos propios debe ser un número" });
+  }
+
+  if (!file?.buffer || !file?.mimetype) {
+    return res
+      .status(400)
+      .send({
+        err: "La imagen del dispositivo el obligatoria, al igual que su tipo MIME",
+      });
   }
 
   // Creating gadget instance
@@ -55,17 +69,50 @@ app.post("/gadgets", upload.single("image"), async (req, res) => {
     image: {
       data: file.buffer,
       mimeType: file.mimetype,
-    }
+    },
   });
 
   // Trying to store gadget instance in the database
   try {
     await gadget.save();
-  } catch(err) {
+  } catch (err) {
     return res.status(500).send({ err: err._message });
   }
 
   return res.status(201).send({});
+});
+
+app.get("/gadgets", async (req, res) => {
+  try {
+    const gadgets = await Gadget.find();
+    const status = gadgets.length ? 200 : 204;
+    return res.status(status).send(gadgets);
+  } catch (err) {
+    return res.status(500).send({ err: err._message });
+  }
+});
+
+app.get("/gadgets/:id", async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  if (!id)
+    return res
+      .status(400)
+      .send({ err: "Se debe proveer un id" });
+  if (id.length != 24)
+    return res
+      .status(400)
+      .send({ err: "El id no está en el formato correcto" });
+
+  try {
+    const gadget = await Gadget.findById(id);
+    const status = gadget ? 200 : 204;
+    return res.status(status).send(gadget);
+  } catch (err) {
+    return res.status(500).send({ err: err._message });
+  }
 });
 
 app.listen(port, () => {
