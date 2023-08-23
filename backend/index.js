43301a229f9e364ee894f04e61b0aef28d9805e0
Francisco Iocco <franciscoiocco6@gmail.com>
2023-08-23 12:02:34 -0300

    Add PUT endpoint towards managing our stock and our delivered gadgets

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ app.post("/gadgets", upload.single("image"), async (req, res) => {
   const gadget = new Gadget({
     name: body.name,
     barcode: body.barcode.slice(0, 6),
-    ownedQuantity: body.ownedQuantity,
+    ownedQuantity: parseInt(body.ownedQuantity),
     givenQuantity: 0,
     image: {
       data: file.buffer,
@@ -82,12 +82,15 @@ app.post("/gadgets", upload.single("image"), async (req, res) => {
 
 app.get("/gadgets", async (req, res) => {
   const { query } = req;
+  // If there is a 'barcode' parameter, it searches for only one type of gadget
+  // Otherwise it does a general search
   try {
     if (query.barcode) {
       if (query.barcode.length < 6)
         return res.status(400).send({
           err: "El código de barras debe contener al menos el tipo de producto y el código del fabricante",
         });
+      // Barcode is sliced as it is only necessary the first six digits (type of product and manufacturer code)
       const barcode = query.barcode.slice(0, 6);
       const gadget = await Gadget.findOne({ barcode });
       if (!gadget)
@@ -119,6 +122,69 @@ app.get("/gadgets/:id", async (req, res) => {
     if (!gadget)
       return res.status(404).send({ err: "El dispositivo no existe" });
     return res.status(200).send(gadget);
+  } catch (err) {
+    return res.status(500).send({ err: err._message });
+  }
+});
+
+app.put("/gadgets/:id", async (req, res) => {
+  const {
+    params: { id },
+    body,
+  } = req;
+
+  // Here we calculate if we are either adding gadgets or delivering gadgets.
+
+  let quantityToProcess = body.ownedQuantity || body.givenQuantity;
+
+  if (!quantityToProcess) {
+    return res.status(400).send({
+      err: "No se enviaron dispositivos para agregar o para entregar",
+    });
+  }
+
+  if (!quantityToProcess > 0) {
+    return res
+      .status(400)
+      .send({ err: "Se debe enviar al menos 1 dispostivo para procesar" });
+  } else if (!parseInt(quantityToProcess)) {
+    return res
+      .status(400)
+      .send({ err: "La cantidad de dispositivos debe ser un número" });
+  }
+
+  if (!id) return res.status(400).send({ err: "Se debe proveer un id" });
+  if (id.length != 24)
+    return res
+      .status(400)
+      .send({ err: "El id no está en el formato correcto" });
+
+  try {
+    const outdatedGadget = await Gadget.findById(id);
+    if (!outdatedGadget)
+      return res.status(404).send({ err: "El dispositivo no existe" });
+    
+    quantityToProcess = parseInt(quantityToProcess);
+
+    // Add gadgets to our amount (if there were)
+    if (body.ownedQuantity) {
+      await Gadget.findByIdAndUpdate(id, {
+        ownedQuantity: outdatedGadget.ownedQuantity + quantityToProcess,
+      });
+    }
+
+    // Substract gadgets to our amount and add them to our 'given' amount (if there were)
+    if (body.givenQuantity) {
+      if (outdatedGadget.ownedQuantity < quantityToProcess)
+        return res
+          .status(400)
+          .send({ err: "No hay suficiente stock para entregar" });
+
+      await Gadget.findByIdAndUpdate(id, {
+        ownedQuantity: outdatedGadget.ownedQuantity - quantityToProcess,
+        givenQuantity: outdatedGadget.givenQuantity + quantityToProcess,
+      });
+    }
   } catch (err) {
     return res.status(500).send({ err: err._message });
   }
